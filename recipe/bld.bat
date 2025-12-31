89d50218cbd9ea3467c67a516dcd9bba3ec6386e
Daniel Nachun <daniel.nachun@gmail.com>
2025-12-31 15:45:26 -0600

    fix Windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
-sed -i 's/PKG_LIBS = /PKG_LIBS = -lpthread ' src/Makevars.ucrt
+sed -i 's/PKG_LIBS = /PKG_LIBS = -lpthread/' src/Makevars.ucrt 
+IF %ERRORLEVEL% NEQ 0 exit /B 1
 
 "%R%" CMD INSTALL --build . %R_ARGS%
 IF %ERRORLEVEL% NEQ 0 exit /B 1
